e251015cbc70d661de71bc2077585e19847278ec
Vijay Sharma <vijay.sharm@gmail.com>
2020-09-30 19:38:13 -0400

    Added TestFairy

--- index.js
+++ index.js
@@ -5,5 +5,7 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+const TestFairy = require('react-native-testfairy');
 
+TestFairy.begin("5b3af35e59a1e074e2d50675b1b629306cf0cfbd");
 AppRegistry.registerComponent(appName, () => App);
